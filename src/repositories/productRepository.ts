5abed51aecb40bb846a1c7ac65b233b606afad08
gunner <gunnersupanut@gmail.com>
2026-01-11 19:39:16 +0700

    feat: Add Size to productRepository(getById)

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -39,26 +39,33 @@ export const productRepository = {
         const sql =
             `
         SELECT
+        pd.id, 
         pd.product_name, 
-        pd.price, 
+        MIN(pv.price) AS price,
         pd.description, 
         pd.image_url,
         c.name AS category,
         (
-            SELECT JSON_AGG(DISTINCT cl.name) 
-            FROM product_variants pv
-            JOIN colors cl ON pv.color_id = cl.id
-            WHERE pv.product_id = pd.id 
-        ) AS available_colors,
-          (
-            SELECT JSON_AGG(DISTINCT si.name) 
-            FROM product_variants pv
-            JOIN sizes si ON pv.size_id = si.id
-            WHERE pv.product_id = pd.id 
-        ) AS available_sizes
-
+        SELECT JSON_AGG(DISTINCT cl.hex_code) 
+        FROM product_variants pv
+        JOIN colors AS cl ON pv.color_id = cl.id
+        WHERE pv.product_id = pd.id 
+        ) AS available_colors_code,     
+        (
+        SELECT JSON_AGG(DISTINCT cl.name) 
+        FROM product_variants pv
+        JOIN colors AS cl ON pv.color_id = cl.id
+        WHERE pv.product_id = pd.id 
+        ) AS available_colors_name,
+        (
+        SELECT JSON_AGG(DISTINCT si.name)
+        FROM product_variants pv
+        JOIN sizes AS si ON pv.size_id = si.id
+        WHERE pv.product_id = pd.id 
+        ) AS available_sizes  
         FROM products AS pd 
         LEFT JOIN categories AS c ON pd.category_id = c.id
+        LEFT JOIN product_variants AS pv ON pv.product_id = pd.id
         WHERE pd.id = $1
         GROUP BY 
         pd.id, c.name
