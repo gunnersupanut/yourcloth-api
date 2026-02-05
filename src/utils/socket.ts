155218bda532c23b9f4a9d836e537fbff9083bc7
gunner <gunnersupanut@gmail.com>
2026-02-05 17:03:09 +0700

    fix:Socket IO

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -6,8 +6,9 @@ let io: Server;
 export const initSocket = (server: http.Server) => {
   io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || "*", 
-      methods: ["GET", "POST"],
+      origin: process.env.CLIENT_URL || "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      credentials: true
     },
   });
   return io;
